d3a21f7d2cd7437c0fa35bf6c60d7e4e9be2a6dc
GeorgeWolf <alfaa.gen@gmail.com>
2015-06-11 19:09:40 +0300

    All works again

---
+++ Assets/SAnimation/SAnimationBaker.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+using UnityEngine;
+
+namespace Assets.SAnimation
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class SAnimationBaker : MonoBehaviour
+    {
+        public string Name = "Factory";
+        public bool Baked;
+        public Sprite[] Sprites;
+
+        public void OnDrawGizmos()
+        {
+            if (Baked)
+            {
+                if (Sprites != null && Sprites.Length > 0 && Name != default(string))
+                {
+
+                    Sprites = Sprites.OrderBy(s => s.name).ToArray(); // Sorting over Array
+                    string[] spriteNames = SpriteNames();
+                    SaveData(spriteNames);
+
+                    SpriteAnimation sa = gameObject.AddComponent<SpriteAnimation>();
+                    sa.FolderName = Name;
+                    Baked = false;
+                    gameObject.GetComponent<SpriteRenderer>().sprite = Sprites[0];
+                    DestroyImmediate(this);
+
+                }
+                else
+                {
+                    Debug.Log("Add elements to array or set the name");
+                    Baked = false;
+                }
+            }
+
+        }
+
+        private void SaveData(string[] spriteNames)
+        {
+
+            CircleLinkedList obj = new CircleLinkedList(spriteNames);
+            XmlSerializer xs = new XmlSerializer(typeof(CircleLinkedList));
+            FileStream fs = File.Create(Environment.CurrentDirectory + @"\Assets\Resources\" + Name + @"\Baked.xml");
+            xs.Serialize(fs, obj);
+            fs.Close();
+        }
+
+        private string[] SpriteNames()
+        {
+            string[] spriteNames = new string[Sprites.Length];
+            for (int i = 0; i < Sprites.Length; i++)
+            {
+                spriteNames[i] = Name + "/" + Sprites[i].name;
+            }
+            return spriteNames;
+        }
+    }
+}
